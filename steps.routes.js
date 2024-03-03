a70f2096bdf2f96a175f75078ccdcb2a3e2fda8a
AnneQuinkenstein <AnneQuinkenstein@users.noreply.github.com>
2024-03-03 22:05:53 +0100

    added left join to get steps without responsible users yet

--- steps.routes.js
+++ steps.routes.js
@@ -6,12 +6,11 @@ const router = express.Router();
 
 // get all Steps for a Milestone
 router.get('/:id', async(req, res) => {
-    const query = `select status, todo, notes, context, nickname from milestones NATURAL JOIN (nextsteps NATURAL JOIN (responsible NATURAL JOIN users)) where milestone_name=$1`;
-    //TODO: outer left join oder sowas, da wo user auch null sein können
+    const query = `select status, todo, notes, context, nickname from milestones NATURAL JOIN (nextsteps NATURAL LEFT JOIN (responsible NATURAL JOIN users)) where milestone_name=$1`;
     console.log(query)
     try {
-        const project_id = req.params.id;
-        const result = await client.query(query, [project_id])
+        const milestone_name = req.params.id;
+        const result = await client.query(query, [milestone_name])
         console.log(result)
         res.send(result.rows);
     } catch (err) {
